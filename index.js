68568b99b42a5b3ee5089389ce90ed50b3196338
Derek Baker <derek.baker89@gmail.com>
2023-08-19 02:01:36 +0100

    GET route returns JSON

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({ message: 'Hello World' })
 })
 
 app.get('/time', (req, res) => {
